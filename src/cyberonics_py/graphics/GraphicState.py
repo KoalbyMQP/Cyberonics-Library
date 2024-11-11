f16482bd1986fc57e31bc4e7c18f1f49e7b7c981
Skyler <skylerwiernik@gmail.com>
2024-11-11 10:49:48 -0500

    UUID fix

--- src/cyberonics_py/graphics/GraphicState.py
+++ src/cyberonics_py/graphics/GraphicState.py
@@ -1,17 +1,22 @@
 import json
+from uuid import UUID, uuid4
+
+
 
 class GraphicState:
-    def __init__(self, graphic_type, uuid, **kwargs):
+    def __init__(self, graphic_type: str, uuid: uuid4, **kwargs):
         self.type = graphic_type
         self.uuid = uuid
         self.__dict__.update(kwargs)
 
     def encode(self):
-        return json.dumps(self.__dict__)
+        state_dict = self.__dict__.copy()
+        state_dict['uuid'] = str(self.uuid)
+        return json.dumps(state_dict)
 
     @staticmethod
     def decode(state_str) -> 'GraphicState':
         state_dict = json.loads(state_str)
         graphic_type = state_dict.pop('type')
-        uuid = state_dict.pop('uuid')
-        return GraphicState(graphic_type, uuid, **state_dict)
+        uuid_obj = UUID(state_dict.pop('uuid'))
+        return GraphicState(graphic_type, uuid_obj, **state_dict)
