a5483d57843539b8efc121a98e709bbff6e2430b
Skyler <skylerwiernik@gmail.com>
2024-11-12 17:16:42 -0500

    Made status dot take a deviceproperty

--- src/cyberonics_py/graphics/cells/StatusDot.py
+++ src/cyberonics_py/graphics/cells/StatusDot.py
@@ -1,10 +1,13 @@
 from ..Graphic import Graphic
 from ..GraphicState import GraphicState
 from ..GraphicTyping import Color
+from ... import DeviceProperty
 
 
 class StatusDot(Graphic):
-    def __init__(self, color: Color) -> None:
+    def __init__(self, color: DeviceProperty[Color]) -> None:
+        if color.type != Color:
+            raise ValueError("color must be of type Color")
         self.__color = color
         self.__flash = False
         super().__init__(None)
@@ -17,16 +20,16 @@ class StatusDot(Graphic):
 
     @property
     def color(self) -> Color:
-        return self.__color
+        return self.__color.value
 
     @color.setter
     def color(self, value: Color) -> None:
-        self.__color = value
+        self.__color.value = value
         super()._notify()
 
 
     def get_state(self) -> GraphicState:
-        return GraphicState("StatusDot", super().uuid, color=self.__color, flash=self.__flash)
+        return GraphicState("StatusDot", super().uuid, color=self.__color.value, flash=self.__flash)
 
     def set_state(self, state: GraphicState) -> None:
         raise ValueError("StatusDot cannot by set by the client")
