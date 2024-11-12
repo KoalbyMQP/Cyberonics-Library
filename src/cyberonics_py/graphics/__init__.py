002e19a0f96a0c42b05421f8ee332e5f588420ab
Skyler <skylerwiernik@gmail.com>
2024-11-12 17:08:38 -0500

    Exposed Color and TextAlignment

--- src/cyberonics_py/graphics/__init__.py
+++ src/cyberonics_py/graphics/__init__.py
@@ -1,5 +1,6 @@
 from .GraphicCell import GraphicCell
 from .Graphic import Graphic
 from .cells import BodyText, Button, HeaderText, Slider, StatusDot, SubText, Switch
+from .GraphicTyping import Color, TextAlignment
 
-__all__ = ["GraphicCell", "Graphic", "BodyText", "Button", "HeaderText", "Slider", "StatusDot", "SubText", "Switch"]
+__all__ = ["GraphicCell", "Graphic", "BodyText", "Button", "HeaderText", "Slider", "StatusDot", "SubText", "Switch", "Color", "TextAlignment"]
