6fb6e8d53e4b2f4b6f25e4af67a87ac1e886d3b0
Skyler <skylerwiernik@gmail.com>
2024-11-29 19:51:02 -0500

    Added targets to robot class

--- src/cyberonics_py/Robot.py
+++ src/cyberonics_py/Robot.py
@@ -7,7 +7,12 @@ from .Target import Target
 class Robot(ABC):
     def __init__(self, devices: Optional[list[Device]], targets: Optional[list[Target]] = None):
         self.__devices = devices or []
+        self.__targets = targets or []
 
     @property
     def devices(self) -> list[Device]:
         return self.__devices
+
+    @property
+    def targets(self) -> list[Target]:
+        return self.__targets
