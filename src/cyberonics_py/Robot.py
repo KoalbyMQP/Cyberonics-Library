74a851596f53ec347296897762be7a9639f0e712
Skyler Wiernik <skyler@Skylers-MacBook-Air-1327.local>
2025-04-01 22:16:28 -0400

    Run targets

--- src/cyberonics_py/Robot.py
+++ src/cyberonics_py/Robot.py
@@ -7,7 +7,7 @@ from .Target import Target
 class Robot(ABC):
     def __init__(self, devices: Optional[list[Device]], targets: Optional[list[Target]] = None):
         self.__devices = devices or []
-        self.__targets = targets or []
+        self.__targets = {t.name: t for t in targets or []}
 
     @property
     def devices(self) -> list[Device]:
@@ -15,4 +15,10 @@ class Robot(ABC):
 
     @property
     def targets(self) -> list[Target]:
-        return self.__targets
+        return self.__targets.values()
+
+    def run_target(self, withName: str):
+        target = self.__targets.get(withName)
+        if not target:
+            raise Exception(f"Target {withName} not found")
+        target.run()
--- src/cyberonics_py/Target.py
+++ src/cyberonics_py/Target.py
@@ -7,9 +7,14 @@ if TYPE_CHECKING:
 
 class Target(ABC):
 
-    def __init__(self, robot: 'Robot', shutdown_timeout: float = 0.5):
+    def __init__(self, name: str, robot: 'Robot', shutdown_timeout: float = 0.5):
+        self._name = name
         self.robot = robot
         self.shutdown_timeout = shutdown_timeout
+
+    @property
+    def name(self) -> str:
+        return self._name
 
     @abstractmethod
     async def run(self):
